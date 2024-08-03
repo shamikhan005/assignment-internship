55baf01be3f0916b086a6293fee4fc1ca132e949
Shamiullah Khan <110717400+shamikhan005@users.noreply.github.com>
2024-08-04 01:47:46 +0530

    Update dbconfig.ts

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -4,7 +4,7 @@ const MONGODB_URI = process.env.MONGO_URI!;
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGO_URI environment variable inside .env.local'
+    'Please define the MONGO_URI environment variable inside .env'
   )
 }
 
