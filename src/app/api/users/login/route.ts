602306cc068b5ede695050b647cac03693845c64
Shamiullah Khan <110717400+shamikhan005@users.noreply.github.com>
2024-08-04 02:45:29 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -4,10 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-connect();
-
 export async function POST(request: NextRequest) {
   try {
+    await connect();
     const reqBody = await request.json();
     const { username, password } = reqBody;
     console.log(reqBody);
