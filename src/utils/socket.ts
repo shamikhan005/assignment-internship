d1e32a585147b6076f40f10d3bd0c144d253b92f
Shamiullah Khan <110717400+shamikhan005@users.noreply.github.com>
2024-08-06 01:10:32 +0530

    Update socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,9 +2,12 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket | null = null;
 
-export const initSocket = async (): Promise<Socket> => {
+export const initSocket = async (userId?: string): Promise<Socket> => {
   if (!socket) {
-    socket = io();
+    socket = io({
+      path: '/socket.io/',
+      query: userId ? { userId } : undefined
+    });
   }
   return socket;
 }
